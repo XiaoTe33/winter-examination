28cd5ecb5633b378fde2db1f8f9d1e8a0018ae46
PIPI <3047409527@qq.com>
2023-02-01 20:34:12 +0800

    4

---
+++ templates/js/commoditydetail.js
@@ -0,0 +1,85 @@
+//放大镜
+const pic = document.querySelector('#pic');
+const bigpic = document.querySelector('#bigpic');
+const mask = document.querySelector('#mask');
+
+pic.addEventListener('mouseenter',()=>{
+    bigpic.style.display = 'block';
+    mask.style.display = 'block';
+})
+pic.addEventListener('mouseleave',()=>{
+    bigpic.style.display = 'none';
+    mask.style.display = 'none';
+})
+
+pic.addEventListener('mousemove',(event)=>{
+    let clientX=event.clientX;
+    let clientY=event.clientY;
+    let eleX=pic.getBoundingClientRect().left;
+    let eleY=pic.getBoundingClientRect().top;
+
+    let left=clientX-eleX-mask.offsetWidth/2;
+    let top=clientY-eleY-mask.offsetHeight/2;
+
+    if(left<=0){
+        left=0;
+    }else if(left>=pic.clientWidth-mask.offsetWidth){
+        left=pic.clientWidth-mask.offsetWidth;
+    }
+
+    if(top<=0){
+        top=0;
+    }else if(top>pic.clientHeight-mask.offsetHeight){
+        top=pic.clientHeight-mask.offsetHeight;
+    }
+    mask.style.left=left+'px';
+    mask.style.top=top+'px';
+    bigpic.scrollLeft=left*2;
+    bigpic.scrollTop=top*2;
+})
+
+
+//加购数量
+const num = document.querySelector('#num')
+const del = document.querySelector('.del1');
+const add = document.querySelector('.add1');
+const buy = document.querySelector('.buyitnow');
+const addtocar = document.querySelector('.addtocar');
+del.addEventListener('click',()=>{ 
+    if(num.value <= 1){
+        console.log('111')
+        del.style.cursor = 'not-allowed';
+        del.style.color = '#dedede';
+    }
+    if(num.value > 1){
+        num.value = num.value - 1;
+    }
+})
+
+add.addEventListener('click',()=>{
+    num.value = Number(num.value)+ 1;
+    if(num.value > 1){
+        console.log('111')
+        del.style.cursor = 'point';
+        del.style.color = '#666666';
+    }
+})
+//立即购买，跳转订单页面
+buy.addEventListener('click',async()=>{
+    const fd = new FormData();
+    fd.append("goodsId","张三");
+    fd.append("amount",num.value);
+    fd.append("style","23");
+    fd.append("address","male");  
+    console.log(fd.get('amount'));
+    const res = await fetch('http://39.101.72.18:9090/order', {
+        method: "post",
+        body: fd,
+      })
+      const data = await res.json();
+      console.log(data);
+})
+//加入购物车
+addtocar.addEventListener('click',async()=>{
+    
+})
