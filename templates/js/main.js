65e6702dda1e233be6a5b67853c9d46e4333b78f
PIPI <3047409527@qq.com>
2023-02-04 05:53:34 +0800

    main

---
+++ templates/js/main.js
@@ -0,0 +1,53 @@
+const leftbtn = document.querySelector('.zuo');
+const rightbtn = document.querySelector('.you');
+const images = document.querySelector('.images');
+//console.log(images.children[0])
+
+// const timer = setInterval(function () {
+//         rightbtn.click();
+//     }, 10000);
+    
+// images.addEventListener('mouseenter', ()=> {
+//         leftbtn.style.display = 'block';
+//         rightbtn.style.display = 'block';
+//         clearInterval(timer);
+//         timer = null;//清除计时器
+//     });
+// images.addEventListener('mouseleave',()=> {
+//         leftbtn.style.display = 'none';
+//         rightbtn.style.display = 'none';
+//         timer = setInterval(function () {
+//             rightbtn.click();
+//         }, 10000);
+//     });
+
+
+// var num = 1;
+// //点击左右按钮
+// rightbtn.addEventListener('click', function () {
+//     // 最后一张图片
+//     if (num == images.children.length) {
+//             num = 0;
+//     }
+//     num++;
+//     //排他清除一下   
+//     for (let i = 0; i < dots.children.length; i++) {
+//         dots.children[i].className = '';
+//         images.children[i].classList.remove = 'current';
+//     }
+//     dots.children[num-1].className = 'active';
+//     images.children[num-1].classList.add('current');
+// })
+
+// leftbtn.addEventListener('click',()=> {
+//     if (num == 0) {
+//          num = images.children.length;
+//      }
+//     num--;
+//     for (let i = 0; i < dots.children.length; i++) {
+//         dots.children[i].className = '';
+//         images.children[i].classList.remove = 'current';
+//     }
+//     dots.children[num].className = 'active';
+//     images.children[num].classList.add('current');
+//     })
