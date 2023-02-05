35254aea6ebd03a8991c6c1299134fbd4d7f62e9
PIPI <3047409527@qq.com>
2023-02-05 17:04:46 +0800

    7

--- templates/js/main.js
+++ templates/js/main.js
@@ -51,7 +51,7 @@
 //     dots.children[num].className = 'active';
 //     images.children[num].classList.add('current');
 //     })
-const phonenum = document.querySelector('#phonenum');
+const phonenum = document.querySelector('.phonenum');
 const example = document.querySelectorAll('.more');
 const header = document.querySelector('.header2');
 var storage = window.localStorage;
@@ -64,6 +64,7 @@ window.addEventListener('load',async()=>{
     })
      let data = await res.json();
      console.log(data);
+     console.log(data.data.phone)
      if( data.status == 200){
        phonenum.innerText = data.data.phone;
        header.children[2].classList.remove('none');
@@ -88,11 +89,101 @@ setInterval(async()=>{
   if(data.status == 200){
       storage.setItem("token",data.token)
   }
-},1000)
+},200000)
 
 // console.log(example);
 // example[0].id = 3055483356794;
 // console.log(example[0].id)
+
+// const imgs = document.querySelectorAll('.img');
+// const left = document.querySelector('.zuo');
+// const right = document.querySelector('.you');
+// const dots = document.querySelector('.dots').querySelector('li');
+
+ 
+//   // // 鼠标移动到左右箭头的位置更换图片 有灰色背景的图片
+//   // leftArrow.addEventListener('mouseenter', function() {
+//   //     this.style.backgroundPosition = '0 0';
+//   // });
+
+//   // leftArrow.addEventListener('mouseleave', function() {
+//   //     this.style.backgroundPosition = '-83px 0';
+//   // });
+
+//   // rightArrow.addEventListener('mouseenter', function() {
+//   //     this.style.backgroundPosition = '-42px 0';
+//   // });
+
+//   // rightArrow.addEventListener('mouseleave', function() {
+//   //     this.style.backgroundPosition = '-123px 0';
+//   // });
+
+
+//   // 给图片设置index 属性，好判断当前的图片是哪一张
+//   for (let i = 0; i < imgs.length; i++) {
+//       imgs[i].setAttribute('data-index', i);
+//   }
+
+//   // 获取当前图片 和 图片的index（数组下标）
+//   var current = this.document.querySelector('.current');
+//   var currentIndex = current.getAttribute('data-index');
+
+//   // 左箭头的点击事件，点击了就返回前一张图片 
+//   // 如果当前图片为第一张那么需要更换到最后一张图片也就是第四张
+//   left.addEventListener('click', function() {
+//       if (currentIndex > 0) {
+//           imgs[currentIndex].classList.remove('current');
+//           dots[currentIndex].classList.remove('active');
+//           imgs[--currentIndex].classList.add('current');
+//           dots[currentIndex].classList.add('active');
+//       } else {
+//           imgs[currentIndex].classList.remove('current');
+//           dots[currentIndex].classList.remove('active');
+//           currentIndex = 4;
+//           imgs[currentIndex].classList.add('current');
+//           dots[currentIndex].classList.add('active');
+//       }
+//   });
+
+//   // 点击右箭头下一张图片切换
+//   // 如果当前为第五张图片，那么切换回第一张图片
+//   right.addEventListener('click', changeImage);
+
+//   var timer = this.setInterval(changeImage, 8000);
+
+//   function changeImage() {
+//       if (currentIndex < 4) {
+//           imgs[currentIndex].classList.remove('current');
+//           dots[currentIndex].classList.remove('active');
+//           imgs[++currentIndex].classList.add('current');
+//           dots[currentIndex].classList.add('active');
+//       } else {
+//           imgs[currentIndex].classList.remove('current');
+//           dots[currentIndex].classList.remove('active');
+//           currentIndex = 0;
+//           imgs[currentIndex].classList.add('current');
+//           dots[currentIndex].classList.add('active');
+//       }
+//   };
+
+//   // 小圆点的点击事件
+//   for (let k = 0; k < dots.length; k++) {
+//       dots[k].setAttribute('data-index', k);
+//       dots[k].addEventListener('click', function() {
+//           var index = this.getAttribute('data-index');
+//           if (index != currentIndex) {
+//               imgs[currentIndex].classList.remove('current');
+//               dots[currentIndex].classList.remove('active');
+//               imgs[index].classList.add('current');
+//               dots[index].classList.add('active');
+//               currentIndex = index;
+//           }
+
+//       })
+//   }
+
+
+
 
 // //注册事件
 for( let i = 0; i < example.length; i++){
--- templates/js/personal.js
+++ templates/js/personal.js
@@ -1,7 +1,11 @@
-const details = document.querySelectorAll('.text')
+const details = document.querySelectorAll('.text');
+const name = document.querySelector('#name')
 var storage = window.localStorage;
+const fileInp = document.querySelector('#fileInp');
+const avator = document.querySelector('.touxiang');
+const pic = document.querySelector('#avator');
+const avatorbtn = document.querySelector('#avatorbtn');
 window.addEventListener('load',async()=>{
-    var storage = window.localStorage;
     //console.log(storage.getItem("token"));
     let url = `http://39.101.72.18:9090/user/info`
     const res = await fetch(url, {
@@ -13,6 +17,8 @@ window.addEventListener('load',async()=>{
      if( data.status == 200){
        details[1].children[0].innerHTML = data.data.phone;
        details[2].children[0].innerHTML = data.data.email;
+       name.innerHTML = data.data.username;
+       pic.src = data.data.photo;
      }
      
 
@@ -29,6 +35,46 @@ setInterval(async()=>{
     let data = await res.json();
     console.log(data);
     if(data.status == 200){
-        storage.setItem("token",data.token)
+        storage.setItem("token",data.token);
     }
-},200000)
+},2000000)
+
+
+avator.onclick = function(){
+  fileInp.click()
+}
+avatorbtn.addEventListener('click',async()=>{
+    if(fileInp.files.length >0){
+          const fd = new FormData();
+        fd.append("photo",fileInp.files[0]);
+        const res = await fetch('http://39.101.72.18:9090/user/photo', {
+            method: "post",
+            headers : {"Token": storage.getItem("token")},
+            body: fd,
+        })
+      const data = await res.json();
+      console.log(data);
+      if( data.status == 200){
+      console.log('222');
+    }
+  }
+})
+
+const push = document.querySelector('#push');
+const del = document.querySelector('#delete');
+const nikname = document.querySelector('#nicheng');
+
+push.addEventListener('click',async()=>{
+        const fd = new FormData();
+        fd.append("username",details[0].children[0].value);
+        fd.append("email",details[2].children[0].innerHTML);
+        fd.append("phone",details[1].children[0].innerHTML);
+        console.log(fd.get("username"));
+        const res = await fetch('39.101.72.18:9090/user/info', {
+            method: "put",
+            headers : {"Token": storage.getItem("token")},
+            body: fd,
+        })
+      const data = await res.json();
+      console.log(data);
+})
