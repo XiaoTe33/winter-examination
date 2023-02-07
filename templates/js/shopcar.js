49aba2a0000e6f6f671fbf8c0e54906341a80399
PIPI <3047409527@qq.com>
2023-02-07 12:54:42 +0800

    购物车

--- templates/js/shopcar.js
+++ templates/js/shopcar.js
@@ -1,5 +1,9 @@
 var storage = window.localStorage;
 let details = document.querySelector('.details');
+const del = document.querySelector('.delete');
+let yes = document.querySelector('.yes');
+let totalPrice = document.querySelector('.totalprice');
+let totalNum = document.querySelector('.totalnum');
 
 window.addEventListener('load',async()=>{
     //console.log(storage.getItem("token"));
@@ -45,12 +49,59 @@ window.addEventListener('load',async()=>{
                         btn2.classList.add('btn');
                         btn2.innerHTML = '+'
                         li.appendChild(btn2);
+                        //在生成的同时绑定事件监听
+                        btn1.addEventListener('click',()=>{ 
+                                if(input.value > 1){
+                                    input.value = input.value - 1;
+                                }
+                                if(input.value <= 1){
+                                    console.log('111');
+                                    //del1[i].style.cursor = 'not-allowed';
+                                    btn1.style.color = '#dedede';
+                                }
+                            })
+                        btn2.addEventListener('click',()=>{
+                            input.value = Number(input.value)+ 1;
+                            if(input.value > 1){
+                                console.log('111')
+                                //del1[i].style.cursor = 'point';
+                                btn1.style.color = '#666666';
+                            }
+                        })
+                        
+
                     }
                     if( j==1){
                         let input = document.createElement('input');
                         input.setAttribute("type","checkbox");
                         input.classList.add('check');
                         li.appendChild(input);
+                        del.addEventListener('click',async()=>{
+                                if( input.checked == true){
+                                    let url = `http://39.101.72.18:9090/user/car/${Number(i)}`
+                                    const res = await fetch(url,{
+                                        method:"delete",
+                                        headers : {"Token": storage.getItem("token")},
+                                    })
+                                    const data = await res.json();
+                                    console.log(data);
+                                    //console.log(url);
+                                }
+                        })
+                        let m = 0;
+                        yes.addEventListener('click',function(){
+                            m++;
+                            if (  m % 2 != 0 ){                               
+                                    input.checked = true;
+                            }else{            
+                                    input.checked = false;
+                            }   
+                        });
+                        // if( input.checked = true){
+                        //     console.log('999')
+                        //     totalNum.innerHTML = Number(totalNum.innerHTML) + Number(num[i]);
+                        //     totalPrice.innerHTML = Number(totalPrice.innerHTML) + Number(sum[i]);
+                        // }
                     }
                     if(j==2){
                         li.classList.add('pic');
@@ -70,6 +121,9 @@ window.addEventListener('load',async()=>{
                         li.innerHTML = Number(data.data[i].price);
                     }
                     if(j==7){li.classList.add('sum')}
+                    if(j==8){
+                        li.innerHTML = '移入关注'
+                    }
                 }
             }
            }
@@ -100,57 +154,52 @@ const num = document.querySelectorAll('.num1');
 const sum = document.querySelectorAll('.sum');
 
 
-
-
-
-//全选 ok
+// //全选 ok
 let input = document.querySelectorAll('.check');
-let yes = document.querySelectorAll('.yes');
-let j=0;
-for(let i = 0; i < yes.length ; i++){
-    yes[i].addEventListener('click',function(){
-        j++;
-        if (  j % 2 != 0 ){
-            for(let i=0;i<input.length;i++){
-                input[i].checked = true;
-            }
-        }else{
-            for(let i=0;i<input.length;i++){             
-                input[i].checked = false;
-            }
-        }   
-    });
-}
+// let yes = document.querySelectorAll('.yes');
+// let j=0;
+// for(let i = 0; i < yes.length ; i++){
+//     yes[i].addEventListener('click',function(){
+//         j++;
+//         if (  j % 2 != 0 ){
+//             for(let i=0;i<input.length;i++){
+//                 input[i].checked = true;
+//             }
+//         }else{
+//             for(let i=0;i<input.length;i++){             
+//                 input[i].checked = false;
+//             }
+//         }   
+//     });
+// }
 
 //更新总价和总数
-let totalPrice = document.querySelector('.totalprice');
-let totalNum = document.querySelector('.totalnum');
-for(let i = 0 ;i< input.length ;i++){  
-    if( input[i].checked = true){
-        console.log('999')
-        totalNum.innerHTML = Number(totalNum.innerHTML) + Number(num[i]);
-        totalPrice.innerHTML = Number(totalPrice.innerHTML) + Number(sum[i]);
-    }
-}
+
+// for(let i = 0 ;i< input.length ;i++){  
+//     if( input[i].checked = true){
+//         console.log('999')
+//         totalNum.innerHTML = Number(totalNum.innerHTML) + Number(num[i]);
+//         totalPrice.innerHTML = Number(totalPrice.innerHTML) + Number(sum[i]);
+//     }
+// }
 
 
 //删除ok
-const del = document.querySelector('.delete');
-del.addEventListener('click',async()=>{
-    for(let i = 0; i<input.length ; i++){
-        if( input[i].checked == true){
-            let url = `http://39.101.72.18:9090/user/car/${Number(i)}`
-            const res = await fetch(url,{
-                method:"delete",
-                headers : {"Token": storage.getItem("token")},
-            })
-            const data = await res.json();
-            console.log(data);
-            //console.log(url);
-        }
+// del.addEventListener('click',async()=>{
+//     for(let i = 0; i<input.length ; i++){
+//         if( input[i].checked == true){
+//             let url = `http://39.101.72.18:9090/user/car/${Number(i)}`
+//             const res = await fetch(url,{
+//                 method:"delete",
+//                 headers : {"Token": storage.getItem("token")},
+//             })
+//             const data = await res.json();
+//             console.log(data);
+//             //console.log(url);
+//         }
 
-    }
-})
+//     }
+// })
 
 //去结算
 const btn = document.querySelector('.totalbtn');
@@ -158,26 +207,26 @@ btn.addEventListener('click',async()=>{
     
 })
 
-//数量加减ok
-const del1 = document.querySelectorAll('.del1');
-const add1 = document.querySelectorAll('.add1');
-for(let i = 0;i < input.length ; i++){
-    del1[i].addEventListener('click',()=>{ 
-        if(num[i].value > 1){
-            num[i].value = num[i].value - 1;
-        }
-        if(num[i].value <= 1){
-            console.log('111');
-            //del1[i].style.cursor = 'not-allowed';
-            del1[i].style.color = '#dedede';
-        }
-    })
-add1[i].addEventListener('click',()=>{
-    num[i].value = Number(num[i].value)+ 1;
-    if(num[i].value > 1){
-        console.log('111')
-        //del1[i].style.cursor = 'point';
-        del1[i].style.color = '#666666';
-    }
-})
-}
+// //数量加减ok
+// const del1 = document.querySelectorAll('.del1');
+// const add1 = document.querySelectorAll('.add1');
+// for(let i = 0;i < input.length ; i++){
+//     del1[i].addEventListener('click',()=>{ 
+//         if(num[i].value > 1){
+//             num[i].value = num[i].value - 1;
+//         }
+//         if(num[i].value <= 1){
+//             console.log('111');
+//             //del1[i].style.cursor = 'not-allowed';
+//             del1[i].style.color = '#dedede';
+//         }
+//     })
+// add1[i].addEventListener('click',()=>{
+//     num[i].value = Number(num[i].value)+ 1;
+//     if(num[i].value > 1){
+//         console.log('111')
+//         //del1[i].style.cursor = 'point';
+//         del1[i].style.color = '#666666';
+//     }
+// })
+// }
